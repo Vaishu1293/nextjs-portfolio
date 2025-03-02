dca6dc260db84897172fa5e7aa2526597282becf
vaish shivkumar <vaish.1231993.vaishali@gmail.com>
2025-03-02 18:12:40 +0000

    spotlight-tailwind-config-error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
     <div className='pb-20 pt-36'>
         <div>
-            
+                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen animate-spotlight' fill="blue"/>
+                <Spotlight className='top-10 left-full h-[80vh] w-[50vw] animate-spotlight' fill="purple"/>
+                <Spotlight className='top-28 left-80 h-[80vh] w-[50vw] animate-spotlight' fill="green"/>
         </div>
     </div>
   )
